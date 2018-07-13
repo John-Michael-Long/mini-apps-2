2f0d88274af5cbbba031727b6c3b2efa71817076
John M. Long <johnm.long@Johns-MacBook-Pro.local>
2018-07-13 11:59:56 -0700

    setup initial dependencies, server, and main react component

---
+++ challenge_3/client/src/js/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return(
+      <div>HELLO FROM APP
+      </div>
+    )
+  }
+}
+
+export default App;
